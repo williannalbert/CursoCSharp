12bc36cb2524182659c9f6779ac3b860dee25aaf
Willian Nalbert <33140454+williannalbert@users.noreply.github.com>
2019-09-12 08:18:47 -0300

    Feita a inclusão dos fundamentos do curso

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using CursoCSharp.EstruturasDeControle;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //fundamentos
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentarios - Fundamentos", Comentarios.Executar},
+                {"Variaveis e constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferência - Fundamentos", Inferencia.Executar},
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto- Fundamentos", Notacao.Executar},
+                {"Lendo dados- Fundamentos", LendoDados.Executar},
+                {"Formatando Número- Fundamentos", FormatandoNumero.Executar},
+                {"Conversões- Fundamentos", Conversoes.Executar},
+                {"Operadores Aritméticos- Fundamentos", OperadoresAritmeticos.Executar},
+                {"Operadores Relacionais- Fundamentos", OperadoresRelacionais.Executar},
+                {"Operadores Lógicos- Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores de Atribuicao- Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários- Fundamentos", OperadoresUnarios.Executar},
+                {"Operadores Ternário- Fundamentos", OperadorTernario.Executar},
+                //Estruturas de controle
+                { "Estrutura If- Esruturas de controle", EstruturaIf.Executar},
+                { "Estrutura IfElse- Esruturas de controle", EstruturaIfElse.Executar},
+                { "Estrutura IfElseIf- Esruturas de controle", EstruturaIfElseIf.Executar},
+                { "Estrutura Switch- Esruturas de controle", EstruturaSwitch.Executar},
+                { "Estrutura While- Esruturas de controle", EstruturaWhile.Executar},
+
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
+
+
+
