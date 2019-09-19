5fc3cded67bc9aef155887aba54ab1b196e136fa
Willian Nalbert <33140454+williannalbert@users.noreply.github.com>
2019-09-19 14:07:54 -0300

    Feito capítulo de Struct

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -41,6 +41,10 @@ namespace CursoCSharp {
                 { "Parametros Nomeados- Classes e Métodos", ParametrosNomeados.Executar},
                 { "Get Set- Classes e Métodos", GetSet.Executar},
                 { "Props- Classes e Métodos", Props.Executar},
+                { "Readonly- Classes e Métodos", Readonly.Executar},
+                { "Enum- Classes e Métodos", ExemploEnum.Executar},
+                { "Struct- Classes e Métodos", ExemploStruct.Executar},
+                { "Struct Vs Classe- Classes e Métodos", StructVsClasse.Executar},
 
             });
 
